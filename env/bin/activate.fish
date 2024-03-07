a93c2301e381df089a588b1be9f4915b8c7aa50a
Ubuntu <ubuntu@ip-172-31-32-129.ec2.internal>
2024-03-07 07:44:46 +0000

    updating

--- env/bin/activate.fish
+++ env/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/patrickwilliamson/Desktop/portal/env"
+set -gx VIRTUAL_ENV "/home/ubuntu/portal/env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
